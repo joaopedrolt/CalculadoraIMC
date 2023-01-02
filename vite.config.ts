8efde3b3f54668a214d7521441ed6cc8a0d09939
João Pedro Lima Teixeira <joaopedrolimateixeira@outlook.com>
2023-01-02 17:59:15 -0300

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/calculadora-imc-reactjs',
+  base: '/calculadora-imc-reactjs/',
   plugins: [react()]
 })
